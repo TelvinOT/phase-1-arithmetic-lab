5872577da048817dbb2563f0dc208cc88cef64cf
Telvin <mark.telvin@student.moringaschool.com>
2024-03-18 21:47:18 +0300

    tyr

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let multiply = ('num1 * num2');
+let num1 =31
+let num2 =2
+let result =62
+multiply =result
+let random = (4,5,6,7)
+random = (4,5,6,7)
 
-// Write your code here
+
+
+let mod = ('num3 / num4')
+let num3 =65
+let num4 =8
+let rem =4
+mod=rem
+
+let max = (2,3,5,20)
+
+
